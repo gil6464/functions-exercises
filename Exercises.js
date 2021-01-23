61969792eac97c0739d0463756a122f4156884ce
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-23 23:08:51 +0200

    bonus qwestion solve! :) and final indetations

--- Exercises.js
+++ Exercises.js
@@ -3,50 +3,50 @@ function myReverse(str) {
   // your code here
   let string = str.split("");
   string.reverse();
+
   let fixString = string.join("");
   return fixString;
-}
+};
 
 //Question 2
 function allCombinations(str) {
-  let combinations = [];
   // your code here
+  let combinations = [];
+
   for(i = 0; i < str.length; i++) {
-    for(j = i+1 ; j < str.length+1; j++) {
+    for(j = i + 1 ; j < str.length+1; j++) {
       combinations.push(str.slice(i, j));
     }
-}
-return combinations;
-
-  //
- 
-}
+};
+      return combinations;
+};
+//
 
 //Question 3
 function allCaps(str) {
   // your code here
     const myArr = str.split(' ');
     const capsLetters = [];
+
     for(i=0; i < myArr.length; i++) {
       capsLetters.push(myArr[i].charAt(0).toUpperCase()
       +myArr[i].slice(1));
-    } 
-  return capsLetters.join(' ');
-}
+    } ;
+      return capsLetters.join(' ');
+};
 
 //Question 4
 function myPower(x, n) {
   // your code here
-  const number = n;
   if (n === 0) {
     return 1;
   } else {
-  for (i=1; i<n; i++) {
+  for (i = 1; i < n; i++) {
     x *= x;
-  }
-  return x;
+  };
+   return x;
  }
-}
+};
 
 
 //Question 5
@@ -58,15 +58,27 @@ function getFirstNotRepeating(str) {
     if (str.indexOf(char) === i && 
     str.indexOf(char, i + 1) === -1) {
       return char;
-    }
-  }
-}
+    };
+  };
+};
 
 //Question 6 (Bonus)
 function isPrefectNumber(num) {
   // your code here
-  return "I'm not interested in the bonus question :(";
-}
+  let counter = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0){
+      counter += i;
+    }
+  };
+    if (counter === num){
+      return true;
+    } else {
+      return false;
+    }
+  return "I'm interested in the bonus question :)";
+};
 
 // *** Playground ***//
 console.log()
